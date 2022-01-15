e5d570cd10afc9d14dc5dbf7b0ffbcc38a210619
doggai10 <doggai77@gmail.com>
2022-01-16 01:09:05 +0900

    add: thread local 적용

---
+++ src/test/java/hello/advanced/trace/thread/FieldServiceTest.java
@@ -0,0 +1,42 @@
+package hello.advanced.trace.thread;
+
+import hello.advanced.trace.thread.local.FieldService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class FieldServiceTest {
+
+    private FieldService fieldService = new FieldService();
+
+    @Test
+    void field(){
+        log.info("main start");
+        Runnable userA = ()->{
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = ()->{
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+        sleep(100);
+        threadB.start();
+        sleep(2000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
